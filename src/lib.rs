1d5e4107b2fe02a3b1659c0cbcf5708c5dd39c53
Shem Sedrick <ssedrick1@gmail.com>
2021-04-09 07:50:24 -0500

    Added h1-h6 support

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use log::{debug, error, info};
-use mdbook::book::{Book, BookItem};
+use mdbook::book::{Book, BookItem, Chapter};
 use mdbook::errors::Error;
 use mdbook::preprocess::{Preprocessor, PreprocessorContext};
 use toml::map::Map;
@@ -10,9 +10,18 @@ enum LineType {
     H1,
     H2,
     H3,
+    H4,
+    H5,
+    H6,
     P,
-    IMG,
 }
+
+const H1_PREFIX: &'static str = "# ";
+const H2_PREFIX: &'static str = "## ";
+const H3_PREFIX: &'static str = "### ";
+const H4_PREFIX: &'static str = "#### ";
+const H5_PREFIX: &'static str = "##### ";
+const H6_PREFIX: &'static str = "###### ";
 
 pub struct PageStyler {
     default_config: Map<String, Value>,
@@ -26,33 +35,105 @@ impl PageStyler {
     }
 
     fn get_line_type(&self, line: &str) -> LineType {
-        if line.starts_with("# ") {
+        if line.starts_with(H1_PREFIX) {
             LineType::H1
-        } else if line.starts_with("## ") {
+        } else if line.starts_with(H2_PREFIX) {
             LineType::H2
-        } else if line.starts_with("### ") {
+        } else if line.starts_with(H3_PREFIX) {
             LineType::H3
+        } else if line.starts_with(H4_PREFIX) {
+            LineType::H4
+        } else if line.starts_with(H5_PREFIX) {
+            LineType::H5
+        } else if line.starts_with(H6_PREFIX) {
+            LineType::H6
         } else {
             LineType::P
         }
+    }
+
+    fn get_class_for<'a>(config: &'a Table, key: &str) -> Option<&'a String> {
+        match config.get(key) {
+            Some(val) => match val {
+                Value::Table(table) => match table.get("class") {
+                    Some(val) => match val {
+                        Value::String(s) => Some(&s),
+                        _ => None,
+                    },
+                    None => None,
+                },
+                _ => None,
+            },
+            None => None,
+        }
+    }
+
+    fn wrap_with_html(
+        line: &str,
+        tag_name: &str,
+        class: &str,
+        strip_prefix: &'static str,
+    ) -> String {
+        format!(
+            "<{0} class=\"{1}\">{2}</{0}>",
+            tag_name,
+            class,
+            line.strip_prefix(strip_prefix).unwrap()
+        )
     }
 
     fn run_chapter(&self, chapter: &Chapter, config: &Table) -> Result<String, Error> {
         let content = chapter
             .content
             .lines()
-            .map(|line| match self.get_line_type(&line) {});
+            .map(|line| match self.get_line_type(&line) {
+                LineType::H1 => {
+                    if let Some(class) = PageStyler::get_class_for(config, "h1") {
+                        PageStyler::wrap_with_html(line, "h1", class, H1_PREFIX)
+                    } else {
+                        line.into()
+                    }
+                }
+                LineType::H2 => {
+                    if let Some(class) = PageStyler::get_class_for(config, "h2") {
+                        PageStyler::wrap_with_html(line, "h2", class, H2_PREFIX)
+                    } else {
+                        line.into()
+                    }
+                }
+                LineType::H3 => {
+                    if let Some(class) = PageStyler::get_class_for(config, "h3") {
+                        PageStyler::wrap_with_html(line, "h3", class, H3_PREFIX)
+                    } else {
+                        line.into()
+                    }
+                }
+                LineType::H4 => {
+                    if let Some(class) = PageStyler::get_class_for(config, "h4") {
+                        PageStyler::wrap_with_html(line, "h4", class, H4_PREFIX)
+                    } else {
+                        line.into()
+                    }
+                }
+                LineType::H5 => {
+                    if let Some(class) = PageStyler::get_class_for(config, "h5") {
+                        PageStyler::wrap_with_html(line, "h5", class, H5_PREFIX)
+                    } else {
+                        line.into()
+                    }
+                }
+                LineType::H6 => {
+                    if let Some(class) = PageStyler::get_class_for(config, "h6") {
+                        PageStyler::wrap_with_html(line, "h6", class, H6_PREFIX)
+                    } else {
+                        line.into()
+                    }
+                }
+                _ => line.into(),
+            })
+            .collect();
         debug!("Content: {:?}", content);
-        for key in chapter_config.keys() {
-            debug!("Key: {}", key);
-            match key.as_str() {
-                "h1" => {}
-                "h2" => {}
-                "h3" => {}
-                _ => error!("Key is not a valid html element name"),
-            }
-        }
-        Ok(chapter.content)
+        Ok(content)
     }
 }
 
@@ -74,12 +155,13 @@ impl Preprocessor for PageStyler {
                 match context.get(&chapter.name) {
                     Some(value) => match value {
                         Value::Table(chapter_config) => {
-                            let new_content = self.run_chapter(&chapter, &chapter_config)?;
+                            let new_content = self.run_chapter(&chapter, &chapter_config).unwrap();
+                            chapter.content = new_content;
                         }
                         _ => error!("Error: Invalid config. Chapter names should be a Toml Table"),
                     },
                     None => {
-                        error!("Error: Invalid config. Chapter names should be in a Toml Table")
+                        info!("Skipping chapter {}: No config values.", chapter.name)
                     }
                 };
             }
@@ -118,7 +200,6 @@ mod tests {
         let mut mdbook = book::MDBook::load("./test-book").unwrap();
         mdbook.with_preprocessor(processor);
         mdbook.build().unwrap();
-
         assert!(true);
     }
 }
